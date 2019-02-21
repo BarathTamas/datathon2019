c7f3d9634426ab583c7dfdd6be7998e0ab174e4b
Tamás Baráth <44137494+BarathTamas@users.noreply.github.com>
2019-02-21 13:15:12 +0100

    Add files via upload

---
+++ data_analysis_tamas/un.R
@@ -0,0 +1,218 @@
+library(dplyr)
+library(tidyr)
+library(tidytext)
+library(ggplot2)
+library(gridExtra)
+library(scales)
+library(reshape2)
+library("wordcloud") # word-cloud generator 
+library("RColorBrewer") # color palettes
+library(stringr)
+library(purrr)
+library(broom)
+library(tidyquant)
+library(pheatmap)
+
+#read in the csv, its too big for github so you have to manually select the data folder
+text_df_raw=read.table("C:/KU Leuven/Datathon/UN/un_raw.csv") %>%
+  mutate(day="09-15") %>%          #for plotting its better to have concrete dates instead of years, the sessions have a
+  unite(date,year,day,sep="-") %>% #moving starting date around the third Tuesday of September, so I approximate with 09.15
+  mutate(date=as.POSIXct(date))    #transforms the year into POSIXct date format, this is best for plotting timeseries
+
+text_df_raw <- text_df_raw %>% mutate_at(4, as.character) #transform texts into string from factor
+
+text_df <- text_df_raw %>% unnest_tokens(word, text)      #splitting into words
+
+
+####finding trending words---------------------
+#count the frequency of words for every year
+words_ts <- text_df %>%
+  mutate(word = str_extract(word, "[a-z']+")) %>%
+  na.omit() %>%
+  anti_join(stop_words) %>% #remove functional words
+  count(date,word) %>%
+  ungroup() %>%
+  group_by(date) %>%
+  mutate(year_total = sum(n)) %>%
+  group_by(word) %>%
+  mutate(word_total = sum(n)) %>%
+  ungroup() %>%
+  filter(word_total > 10000) #only consider words that appear more than 10000 times
+
+#fitting glm to find trends
+nested_data <- words_ts %>%
+  nest(-word)
+nested_models <- nested_data %>%
+  mutate(models = map(data, ~ glm(cbind(n, year_total-n) ~ date, .,
+                                  family = "binomial")))
+#most significant trends
+slopes <- nested_models %>%
+  unnest(map(models, tidy)) %>%
+  filter(term == "date") %>%
+  mutate(adjusted.p.value = p.adjust(p.value)) %>%
+  filter(adjusted.p.value < 0.01) %>%  #only take words that are p<1%
+  select(word, estimate, adjusted.p.value)
+
+top_slopes <- slopes %>% top_n(-50)
+
+top_words_ts <- words_ts %>%
+  select(date,word,n) %>% 
+  filter(word %in% top_slopes$word) %>% 
+  spread(key = word, value = n)
+word_corr <- cor(top_words_ts[,2:50])
+
+#correlated trends
+pal=colorRampPalette(rev(brewer.pal(11,"RdYlBu")))(100)
+pheatmap(word_corr,color=pal,treeheight_row = 0, treeheight_col = 0)
+
+#custom selection of trending words
+my_words1=data_frame(word=c("africa","disarmament","terrorism","independence","poverty",
+                            "powers","south","financial","arab","military",
+                            "east","negotiations","developing","conflict","israel","middle","nuclear"))
+
+my_words2=data_frame(word=c("arab","israel","arms","middle","force"))
+
+my_words3=data_frame(word=c("terrorism","independence","financial","nuclear"))
+
+#plotting the trends
+words_ts %>%
+  inner_join(my_words3, by = "word") %>%
+  ggplot(aes(date, n, color = word)) +
+  geom_line(size = 1.3) +
+  labs(x = NULL, y = "Word frequency") +
+  scale_x_datetime(breaks = seq(as.POSIXct("1970-01-01"),
+                                as.POSIXct("2015-12-31"), "4 years"),
+                   minor_breaks=seq(as.POSIXct("1970-01-01"),
+                                    as.POSIXct("2015-12-31"), "1 years"),
+                   expand = c(0, 0),
+                   limits = c(as.POSIXct("1969-12-01"),
+                              as.POSIXct("2016-01-31"))) +
+  theme_bw()+
+  theme(panel.grid.minor.y = element_blank())
+
+#save the dataframes for the webapp
+write.table(words_ts,"shiny app/words_ts.csv",row.names=F)
+write.table(top_slopes["word"],"shiny app/trending words.csv",row.names=F)
+write.table(word_corr,"shiny app/word_correlations.csv")
+
+
+#who used the words the most in a given year?
+keyword="financial"
+keydate=as.POSIXct("1998-09-15")
+result <- text_df %>%
+  filter(word==keyword) %>%
+  filter(date==keydate) %>% 
+  count(country)
+
+keycountries=result %>%
+  top_n(4) %>% 
+  select(country) %>% 
+  mutate_at(1, as.character)
+
+#extracting sentences of interest with the keyword
+text_df2 <-text_df_raw %>%
+  unnest_tokens(sentence, text, token="sentences") %>%  #tokenize into sentences
+  filter(nchar(sentence)>10)  #remove errors
+
+keytext <- text_df2 %>% 
+  filter(date==keydate) %>% 
+  filter(country %in% keycountries$country) %>%
+  filter(grepl(keyword,sentence))
+
+####session info------------------
+
+session_theme <- read.table("shiny app/session_descr.csv",header = TRUE, stringsAsFactors = FALSE)
+
+session_info=tibble(
+  date=seq(as.POSIXct("1970-09-15"),as.POSIXct("2015-09-15"), "1 years"),
+                    session=as.integer(seq(25,70,1))) %>%
+  left_join(session_theme,by="session") %>% 
+  mutate(link=paste("http://research.un.org/en/docs/ga/quick/regular/",session,sep=""))
+
+write.table(session_info, "shiny app/session_info.csv")
+
+
+
+
+
+
+
+#--------------not needed-----------------
+#financial crisis
+sp500 <- tq_get(x = "^GSPC",from="1970-01-01",to="2016-01-01") %>% 
+  mutate(date=as.POSIXct(date))
+
+sp500_y <- sp500 %>%
+  tq_transmute(select     = adjusted,
+               mutate_fun = periodReturn,
+               type       = "log",
+               period     = "yearly")
+
+
+p1 <- sp500 %>%
+  ggplot(aes(as.POSIXct(date), adjusted)) +
+  geom_line(size = 1.3,color="#5b92e5") +
+  scale_x_datetime(breaks = seq(as.POSIXct("1970-01-01"),
+                                as.POSIXct("2015-12-31"), "4 years"),
+                   minor_breaks=seq(as.POSIXct("1970-01-01"),
+                                    as.POSIXct("2015-12-31"), "1 years"),
+                   expand = c(0, 0),
+                   limits = c(as.POSIXct("1969-12-01"),
+                              as.POSIXct("2016-01-31"))) +
+  theme_bw()+
+  theme(panel.grid.minor.y = element_blank())
+p2 <- filter(words_ts,word=="financial") %>%
+  ggplot(aes(as.POSIXct(date), n)) +
+  geom_line(size = 1.3,color="#5b92e5") +
+  geom_point(size=5,alpha=0.5,color="dark blue")+
+  scale_x_datetime(breaks = seq(as.POSIXct("1970-01-01"),
+                                as.POSIXct("2015-12-31"), "4 years"),
+                   minor_breaks=seq(as.POSIXct("1970-01-01"),
+                                    as.POSIXct("2015-12-31"), "1 years"),
+                   expand = c(0, 0),
+                   limits = c(as.POSIXct("1969-12-01"),
+                              as.POSIXct("2016-01-31"))) +
+  theme_bw()+
+  theme(panel.grid.minor.y = element_blank())
+grid.arrange(p1,p2,nrow=2)
+
+un_df <- text_df %>%
+  mutate(word = str_extract(word, "[a-z']+")) %>%
+  anti_join(stop_words) %>% #removing function words
+  count(word, sort = TRUE)
+#maybe united, nations and international should be removed as well
+
+un_df[1:20,] %>%
+  mutate(word = reorder(word, n)) %>% #reorder neccesary as ggplot doesnt care about sorting
+  ggplot(aes(word, n,label=word)) +
+  geom_col(fill="#5b92e5",color="#5b92e5") +
+  xlab(NULL) +
+  ylab(NULL) +
+  geom_text(size = 5, position = position_stack(vjust = 0.5),
+            color="white",fontface = "bold") +
+  coord_flip() +
+  theme_bw() +
+  theme(axis.title.y=element_blank(),
+        axis.text.y=element_blank(),
+        axis.ticks.y=element_blank())
+
+
+wordcloud(words = un_df$word, freq = un_df$n,
+          max.words=20, random.order=FALSE, 
+          colors=brewer.pal(8, "Dark2"))
+
+
+
+#tf-idf
+un_idf <- text_df %>%
+  mutate(word = str_extract(word, "[a-z']+")) %>%
+  group_by(date) %>%
+  count(date,word) %>%
+  bind_tf_idf(word, date, n) %>%
+  arrange(desc(tf_idf))
+
+ts_idfs <- un_idf %>%
+  top_n(5) %>%
+  arrange(desc(date,tf_idf))
+#--------------------------------
+
---
+++ shiny app/app.R
@@ -0,0 +1,337 @@
+
+# packages ------------------------------------------------------------------------------------
+
+library(tidyverse)
+library(tidytext)
+library(shiny)
+library(shinydashboard)
+library(shinythemes)
+# library(shinytest)
+# library(DT)
+# library(wordcloud)
+# library(ggiraph)
+# library(shinyjqui)
+library(shinyWidgets)
+library(shinycssloaders)
+# library(colourpicker)
+library(formattable)
+library(Cairo)
+library(RColorBrewer)
+library(pheatmap)
+
+# shiny dashboard -----------------------------------------------------------------------------
+
+options(shiny.usecairo = TRUE)
+
+# input data ----------------------------------------------------------------------------------
+
+words_ts <- read.table("words_ts.csv", header = TRUE, stringsAsFactors = FALSE) %>% mutate(date=as.POSIXct(date)) #date column as POSIXct from the start
+
+trending_words <- read.table("trending words.csv", header = TRUE, stringsAsFactors = FALSE)
+
+word_corrs <- read.table("word_correlations.csv", header = TRUE, stringsAsFactors = FALSE)
+
+# ui ------------------------------------------------------------------------------------------
+
+ui <- dashboardPage(
+  
+  skin = "blue",
+  
+  #### header ####
+  
+  dashboardHeader(
+    
+    title = "UN Debate Web App"
+    
+  ),
+  
+  #### sidebar ####
+  
+  dashboardSidebar(
+    
+    sidebarMenu(id = "sidebar",
+                
+                menuItem("Trending Words:", tabName = "dashboard", icon = icon("chart-line", lib = "font-awesome")),
+                menuItem("Dashboard 2:", tabName = "dashboard2", icon = icon("chart-line", lib = "font-awesome")),
+                menuItem("Dashboard 3:", tabName = "dashboard3", icon = icon("chart-line", lib = "font-awesome")),
+                menuItem("About us:", tabName = "aboutus", icon = icon("question", lib = "font-awesome"))
+                
+    ),
+    
+    hr(),
+    
+    #### sliders DB 1 ####
+    
+    conditionalPanel(condition = "input.sidebar=='dashboard'",
+                     
+                     pickerInput(
+                       inputId = "selectedWords",
+                       label = "Which words should be plotted?",
+                       choices = trending_words$word,
+                       selected = c("arab","israel","arms","middle","force,"),
+                       options = list(
+                         `actions-box` = TRUE,
+                         size = 10,
+                         `selected-text-format` = "count > 3"
+                       ),
+                       multiple = TRUE
+                     )
+    ),
+    
+    #### sliders DB 2 ####
+    
+    conditionalPanel(condition = "input.sidebar=='dashboard2'"
+                     
+                     
+                     
+    ),
+    
+    #### sliders DB 3 ####
+    
+    conditionalPanel(condition = "input.sidebar=='dashboard3'"
+                     
+    )
+    
+  ),
+  
+  #### body ####
+  
+  dashboardBody(
+    
+    #### css layout ####
+    
+    tags$head(tags$style(HTML('
+                              /* logo */
+                              .skin-blue .main-header .logo {
+                              background-color: #5b92e5;
+                              }
+                              
+                              /* logo when hovered */
+                              .skin-blue .main-header .logo:hover {
+                              background-color: #5b92e5;
+                              }
+                              
+                              /* navbar (rest of the header) */
+                              .skin-blue .main-header .navbar {
+                              background-color: #5b92e5;
+                              }
+                              
+                              /* main sidebar */
+                              .skin-blue .main-sidebar {
+                              background-color: #ffffff;
+                              }
+                              
+                              /* active selected tab in the sidebarmenu */
+                              .skin-blue .main-sidebar .sidebar .sidebar-menu .active a{
+                              background-color: #5b92e5;
+                              color: #ffffff;
+                              }
+                              
+                              /* other links in the sidebarmenu */
+                              .skin-blue .main-sidebar .sidebar .sidebar-menu a{
+                              background-color: #ffffff;
+                              color: #000000;
+                              }
+                              
+                              /* other links in the sidebarmenu when hovered */
+                              .skin-blue .main-sidebar .sidebar .sidebar-menu a:hover{
+                              background-color: #0A366B;
+                              color: #ffffff;
+                              }
+                              /* toggle button when hovered  */
+                              .skin-blue .main-header .navbar .sidebar-toggle:hover{
+                              background-color: #0A366B;
+                              }
+                              .box.box-solid.box-primary>.box-header {
+                              color:#fff;
+                              }
+                              '))),
+    
+    tabItems(
+      
+      #### content DB 1 ####
+      
+      tabItem(tabName = "dashboard",
+              
+              fluidRow(
+                
+                box(title = "Correlation Heatmap",
+                    status = "info",
+                    width = 5,
+                    height = "550px",
+                    solidHeader = TRUE,
+                    
+                    
+                    withSpinner(plotOutput("corrPlot"))
+                    
+                ),
+                
+                box(title = "Words over Time",
+                    status = "info",
+                    width = 7,
+                    height = "550px",
+                    solidHeader = TRUE,
+                    
+                    withSpinner(plotOutput("wordsOverTime", click = "plot1_click"))
+                )
+              ),
+              
+              fluidRow(
+                
+                box(title = "Information Table",
+                    status = "info",
+                    width = 7,
+                    solidHeader = TRUE,
+                    
+                    withSpinner(dataTableOutput("click_info"))
+                    
+                ),
+                
+                box(title = "Text",
+                    status = "info",
+                    width = 5,
+                    solidHeader = TRUE
+                    
+                    # dataTableOutput("click_info")
+                    
+                )
+                
+              )
+      ),
+      
+      #### content DB 2 ####
+      
+      tabItem(tabName = "dashboard2",
+              
+              fluidRow(
+                
+                valueBoxOutput("testInfo1"),
+                withSpinner(valueBoxOutput("testInfo2")),
+                valueBoxOutput("testInfo3")
+                
+              ),
+              
+              fluidRow(
+                
+                box(title = "Line Plot",
+                    status = "info",
+                    width = 6,
+                    solidHeader = TRUE
+                    
+                    
+                    
+                ),
+                
+                box(title = "Bar Plot",
+                    status = "info",
+                    width = 3,
+                    solidHeader = TRUE
+                    
+                    
+                    
+                ),
+                
+                box(title = "Wordcloud",
+                    status = "info",
+                    width = 3,
+                    solidHeader = TRUE
+                    
+                    
+                    
+                )
+              )
+              
+      ),
+      
+      
+      #### about us ####
+      
+      
+      tabItem(tabName = "aboutus",
+              
+              includeMarkdown("aboutUs.Rmd")
+              
+      )
+    )
+    )
+    )
+
+
+# server --------------------------------------------------------------------------------------
+
+server <- function(input, output) {
+  
+  #### rendered objects DB 1 ####
+  
+  filteredWords <- reactive({
+    words_ts %>% 
+      filter(word %in% input$selectedWords)
+  })
+  
+  output$wordsOverTime <- renderPlot({
+    ggplot(data = filteredWords(), aes(date, n, color = word)) +
+      geom_line(size = 1.3) +
+      geom_point(size = 5, alpha = 0.5) +
+      scale_x_datetime(breaks = seq(as.POSIXct("1970-01-01"),
+                                    as.POSIXct("2015-12-31"), "4 years"),
+                       minor_breaks = seq(as.POSIXct("1970-01-01"),
+                                          as.POSIXct("2015-12-31"), "1 years"),
+                       expand = c(0, 0),
+                       limits = c(as.POSIXct("1969-12-01"),
+                                  as.POSIXct("2016-01-31"))) +
+      scale_color_discrete(name = "Selected Words:") +
+      xlab("Year") +
+      ylab("Frequency") +
+      theme_bw() +
+      theme(legend.position = "bottom",
+            panel.grid.minor.y = element_blank())
+  }, height = 490, width = 735)
+  
+  output$corrPlot <- renderPlot({
+    
+    pal <- colorRampPalette(rev(brewer.pal(11, "RdYlBu")))(100)
+    pheatmap(word_corrs, color = pal, treeheight_row = 0, treeheight_col = 0)
+    
+  }, height = 490, width = 520)
+  
+  output$click_info <- renderDataTable({
+    
+    session_info %>%
+      filter(date==as.POSIXct(nearPoints(filteredWords(), input$plot1_click)[1,"date"]))
+    
+  }, options = list(searching = FALSE, paging = FALSE))
+  
+  #### rendered objects DB 2 ####
+  
+  output$testInfo1 <- renderValueBox({
+    valueBox(
+      subtitle = "Sentiment Index", 
+      value = "Der Gerät wird nie müde", 
+      icon = icon("thumbs-up", lib = "glyphicon"),
+      color = "navy"
+    )
+  })
+  
+  output$testInfo2 <- renderValueBox({
+    valueBox(
+      subtitle = "Mentiod the most positively by:", 
+      value = "Der Gerät schläft nie ein", 
+      icon = icon("thumbs-up", lib = "glyphicon"),
+      color = "blue"
+    )
+  })
+  
+  output$testInfo3 <- renderValueBox({
+    valueBox(
+      subtitle = "Mentiod the most negatively by:", 
+      value = "und ist immer vor chef in geschäft", 
+      icon = icon("thumbs-up", lib = "glyphicon"),
+      color = "light-blue"
+    )
+  })
+  
+}
+
+# app -----------------------------------------------------------------------------------------
+
+shinyApp(ui = ui, server = server)
