5627d78010f7dc647bca5934ba989df32d4cb046
Tamás Baráth <tamas.barath@yahoo.com>
2019-02-22 17:53:29 +0100

    fixed version repush

--- shiny app/app.R
+++ shiny app/app.R
@@ -200,6 +200,9 @@ ui <- dashboardPage(
                               .box.box-solid.box-primary>.box-header {
                               color:#fff;
                               }
+
+                              
+                              
                               
                               .box.box-solid.box-primary>.box-header {
                               color:#fff;
@@ -212,9 +215,7 @@ ui <- dashboardPage(
                               border-right-color:#5b92e5;
                               border-top-color:#5b92e5;
                               }
-                              ')
-    )
-    ),
+                              '))),
     
     tabItems(
       
@@ -230,7 +231,7 @@ ui <- dashboardPage(
                     height = "520px",
                     solidHeader = TRUE,
                     
-                    withSpinner(highchartOutput("corrPlot", width = 480, height = 480))
+                    withSpinner(highchartOutput("corrPlot"))
                     
                 ),
                 
@@ -249,44 +250,42 @@ ui <- dashboardPage(
                 
                 box(title = "Information About Selected Session",
                     status = "primary",
-                    width = 9,
+                    width = 7,
                     solidHeader = TRUE,
                     
                     withSpinner(tableOutput("click_info"))
-                ),
-                    box(title = "Frequency by Country (Top Users)",
-                        status = "primary",
-                        width = 3,
-                        solidHeader = TRUE,
-                        
-                        withSpinner(tableOutput("click_info2"))
-                        
-                    )
+                    
+                )
               ),
               
               fluidRow(
                 
+                box(title = "Frequency by Country (Top Users)",
+                    status = "primary",
+                    width = 3,
+                    solidHeader = TRUE,
+                    
+                    withSpinner(tableOutput("click_info2"))
+                    
+                ),
+                
                 box(title = "Find in Text",
                     status = "primary",
-                    width = 12,
+                    width = 3,
                     solidHeader = TRUE,
                     withSpinner(tableOutput("click_info3")),
                     uiOutput('countries'),
                     actionButton(
                       "lookup", "Lookup"
-                    ),
+                ),
                     
-                    bsModal(id = "wordquote",
+                 bsModal(id = "wordquote",
                             title = "Quote(s) in which the word appeared",
                             trigger = "lookup"
-<<<<<<< HEAD
                  )
                     
-=======
-                            
-                    )
->>>>>>> 2ea5371cf0873320fb1b71dd14f011a2c85dbc70
                 )
+                
               )
       ),
       
@@ -392,14 +391,14 @@ server <- function(input, output) {
     
     pal <- colorRampPalette(rev(brewer.pal(11, "RdYlBu")))(100)
     #pheatmap(word_corrs, color = pal, treeheight_row = 0, treeheight_col = 0)
-    
+
     hc <- hchart(as.matrix(word_corrs_ordered)) %>%
       hc_size(height = 450, width = 480) %>% 
       hc_xAxis(tickmount = 50) %>%
       hc_yAxis(tickmount = 50)
-    
+      
     hc$x$hc_opts$colorAxis$stops <- NULL
-    
+      
     hc %>% 
       hc_colorAxis(stops = color_stops(11, colors=pal))
     
@@ -474,12 +473,12 @@ server <- function(input, output) {
   # sentiment index
   
   sentimentScore <- reactive({
-    
+
     sentiment_data %>%
       filter(year == input$year & country == input$country) %>%
       select(totalscore) %>%
       mutate(totalscore = round(totalscore, 2))
-    
+
   })
   
   output$testInfo1 <- renderValueBox({
