d43118300ab311601daec83726ff354d3e62d2d7
pascalw20 <pascalwiltschko@gmail.com>
2019-02-22 13:56:39 +0100

    update

--- shiny app/app.R
+++ shiny app/app.R
@@ -97,7 +97,9 @@ ui <- dashboardPage(
                          maxOptions = 10
                        ),
                        multiple = TRUE
-                     )
+                     ),
+                     
+                     submitButton("Apply Changes", icon("refresh"), width = "100%")
     ),
     
     #### sliders DB 2 ####
